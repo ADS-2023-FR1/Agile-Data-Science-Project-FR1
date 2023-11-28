6659faa2b4569970f3966d2ff3818bacd3cba5e4
Alejandro Astruc <astruc.lopez.alejandro@gmail.com>
2023-11-28 12:15:18 +0100

    Selecting the best model and adding metrics function to the pipeline.

--- pipeline/settings_definition.py
+++ pipeline/settings_definition.py
@@ -8,8 +8,8 @@ def define_model(config):
     elif config["model"] == "factorizer" and config["submodel"] == "explicit":
         from spotlight.factorization.explicit import ExplicitFactorizationModel as ImportModel
     else:
-        raise Exception(f"Incorrect model configuration for model {config["model"]} and submodel {config["submodel"]}.")
-    return ImportModel(loss=config["loss"], representation=config["representation"], embedding_dim=config["embedding_dim"],
+        raise Exception(f"Incorrect model configuration for model {config['model']} and submodel {config['submodel']}.")
+    return ImportModel(loss=config["loss"], representation=config["representation"],embedding_dim=config["embedding_dim"],
                        n_iter=config["n_iter"], batch_size=config["batch_size"], l2=config["l2"], learning_rate=config["lr"],
                        optimizer_func=config["optim"], use_cuda=config["use_cuda"], sparse=config["sparse"], 
                        random_state=config["random_state"])#, num_negative_samples=config["num_negative_samples"])
@@ -27,5 +27,22 @@ def define_dataset(config):
     elif config["dataset"] == "Goodbooks":
         return get_goodbooks_dataset()
     else:
-        raise Exception(f"Incorrect dataset configuration for dataset {config["dataset"]}.")
+        raise Exception(f"Incorrect dataset configuration for dataset {config['dataset']}.")
+        
+def evaluate_model(model, test, config):
+    """This function evaluates the metric agreed by the team. The metrics that are abailble
+       for all the models are precision, recall, and mmr."""
+    from spotlight.evaluation import mrr_score, precision_recall_score, rmse_score, sequence_mrr_score, sequence_precision_recall_score
+    if config['model']=='factorizer':
+        eval = {"mrr": mrr_score(model, test), "precision_recall": precision_recall_score(model, test), "rmse": rmse_score(model, test),
+                #"sequence_mrr":sequence_mrr_score(model, test), "sequence_precision_recall": sequence_precision_recall_score(model, test)
+               }
+    else:
+        eval = {"mrr": sequence_mrr_score(model, test), "precision_recall": sequence_precision_recall_score(model, test, k=5)
+               }
+    
+    eval.update({"mean_precision": eval['precision_recall'][0].mean(), "precision_std": eval['precision_recall'][0].std(), 
+                 "mean_recall":eval['precision_recall'][1].mean(), "recall_std": eval['precision_recall'][1].std(), 
+                 "mrr_mean": eval['mrr'].mean(), "mrr_std": eval['mrr'].std()})
+    return eval
 
