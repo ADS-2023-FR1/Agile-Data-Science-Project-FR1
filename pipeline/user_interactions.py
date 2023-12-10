368077adaa3f92ca1eb6473f134225068bf67648
Alejandro Astruc <astruc.lopez.alejandro@gmail.com>
2023-12-10 13:57:47 +0100

    Added the recommend new factorial function for the mock user

--- pipeline/user_interactions.py
+++ pipeline/user_interactions.py
@@ -107,3 +107,18 @@ def update_existing_user(data, movie_ids, ratings, user):
     data.ratings = np.concatenate((data.ratings,ratings))
 
     return data
+    
+def recommend_new_factorial(user_id, model_path, n_recom=5):
+    """
+    Receives a trained model path, a sequence of movie titles,
+    associates them to a movielens ID
+    and then predicst n_recoms new movies
+    for the user and returns their titles.
+    """
+    spotlight_model = torch.load(model_path) # Load model
+    
+    pred = spotlight_model.predict(user_id) # In the prediction we could specify a specific subpool in future filter implemtation
+    indices = np.argpartition(pred, -n_recom)[-n_recom:] 
+    best_movie_ids = indices[np.argsort(pred[indices])]
+    titles = [get_movie_title(b+1) for b in best_movie_ids]
+    return titles
