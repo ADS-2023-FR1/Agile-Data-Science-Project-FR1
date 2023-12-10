4b22b5aa28b35d617aeb39e8d62e8f93fb04a396
joel dieguez <jaajaaje@gmail.com>
2023-12-10 16:34:52 +0100

    rebase

---
+++ pipeline/delete.py
@@ -0,0 +1,16 @@
+import os
+import torch
+import numpy as np
+from user_interactions import *
+from settings_definition import *
+
+model_path = "../pipeline/trained_model"
+n_recom = 5
+movies = np.array(['Sabrina', 'Mortal Kombat'])
+
+os.chdir("../spotlight")
+torch.load(model_path)
+titles = recommend_new_sequence(movies, model_path, n_recom=n_recom)
+
+assert len(titles) == n_recom
+print("everythin fine")
