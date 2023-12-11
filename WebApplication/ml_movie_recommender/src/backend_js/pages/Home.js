f4a0a6d502b1ab541894c53428578783366d0ac4
Max von Klinski <Maximilian.VonKlinski@student.hpi.uni-potsdam.de>
2023-12-11 16:46:15 +0100

    Added the navigation from home/landing page to the recommendation page

--- WebApplication/ml_movie_recommender/src/backend_js/pages/Home.js
+++ WebApplication/ml_movie_recommender/src/backend_js/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Landing() {
   return (
     <div>
       <h1>Welcome to our Movie Recommender!</h1>
       <p>Start exploring and discovering new movies.</p>
-      <button>Get Started</button>
+      <Link to="/recommendation">
+        <button>Get Started</button>
+      </Link>
     </div>
   );
 }
